ebc54a0528954dccb958bebe585e5842d87308f3
Chatterjee <ritwichatterjee@deloitte.com>
2026-09-04 00:32:44 +0530

    090426

--- 090326.cpp
+++ 090326.cpp
@@ -1,11 +1,31 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int maxNiceDivisors(int primeFactors) {
+int maxArea(vector<int>& height) {
+    int n = height.size();
+
+    int left = 0, right = n - 1, area = INT_MIN;
+
+    while(left <= right) {
+        area = max(area, (right - left) * min(height[left], height[right]));
+
+        if(height[left] <= height[right]) {
+            left++;
+        } else {
+            right--;
+        }
+    }
+
+    return area;
+}
+
+int minimumDifference(vector<int>& nums) {
     
 }
 
 int main() {
-    cout << maxNiceDivisors(8) << endl;
+    vector<int> nums = {3,9,7,3};
+
+    cout << minimumDifference(nums) << endl;
     return 0;
 }
